36894a9c61d6a74dc2a446d62e5543eb5f642bf1
ezedin <ezedin.fedlu@gmail.com>
2020-08-15 01:27:32 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
   IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
